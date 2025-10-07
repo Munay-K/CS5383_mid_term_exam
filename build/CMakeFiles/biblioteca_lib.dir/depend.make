71fe1d383b886aacf6233b31b4ffe57caf9ea383
Munay-K <gonzalo.alfacaso@gmail.com>
2025-10-07 17:40:29 -0500

    complete remmove and gitignore

--- build/CMakeFiles/biblioteca_lib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for biblioteca_lib.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/munay/code/utec/pruebas_verificacion/CS5383_mid_term_exam
-# Build directory: /home/munay/code/utec/pruebas_verificacion/CS5383_mid_term_exam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(biblioteca_tests "/home/munay/code/utec/pruebas_verificacion/CS5383_mid_term_exam/build/tests")
-set_tests_properties(biblioteca_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/munay/code/utec/pruebas_verificacion/CS5383_mid_term_exam/CMakeLists.txt;44;add_test;/home/munay/code/utec/pruebas_verificacion/CS5383_mid_term_exam/CMakeLists.txt;0;")
